c441c13403e12ddcfe693c776944d707bf75e714
tianshaojun <33224748+tianshaojun@users.noreply.github.com>
2020-09-26 16:40:42 +0800

    保存对话框

---
+++ ElectronDemo10/main.js
@@ -0,0 +1,38 @@
+var electron = require('electron');  //引入electron模块
+
+var app = electron.app;  //创建electron引用
+
+var BrowserWindow = electron.BrowserWindow;  //创建窗口引用
+
+var mainWindow = null;  //声明要打开的主窗口
+
+app.on('ready', ()=>{
+    mainWindow = new BrowserWindow({
+        width:1400,
+        height:600,
+        webPreferences: {
+            nodeIntegration:true,   //允许渲染进程中使用node
+            enableRemoteModule:true //允许渲染进程中使用remote
+        } 
+    });
+
+    require('./main/menu.js');  //加载自定义菜单
+
+    mainWindow.webContents.openDevTools();  //默认开启调试控制台
+
+    mainWindow.loadFile('index.html');  //加载那个页面
+
+
+    //在主进程中用BrowserView嵌入网页
+    // var BrowserView = electron.BrowserView;  //引入BrowserView
+    // var view = new BrowserView();  //new出对象
+    // mainWindow.setBrowserView(view);  //在主窗口中设置view可用
+    // view.setBounds({x:0,y:150,width:1200,height:800});  //定义view的具体样式和位置
+    // view.webContents.loadURL('http://jspang.com');  //view载入的页面
+
+    //监听关闭事件，把主窗口设置为null
+    mainWindow.on('closed', ()=>{
+        mainWindow = null;
+    })
+})
+
---
+++ ElectronDemo10/renderer/index.js
@@ -0,0 +1,94 @@
+const btn = this.document.querySelector('#btn');
+const { BrowserWindow } = require('electron').remote;
+
+window.onload = function(){
+    btn.onclick = ()=>{
+
+        newWin = new BrowserWindow({
+            width:500,
+            height:500,
+        })
+        
+        newWin.loadFile('yellow.html');
+        newWin.on('close', ()=>{win=null});
+    }
+}
+
+
+//创建右键菜单
+const { remote } = require('electron');
+var rightTemplate = [
+    {label:'粘贴'},
+    {label:'复制'}
+]
+
+var m = remote.Menu.buildFromTemplate(rightTemplate);
+
+window.addEventListener('contextmenu', function(e){
+
+    //阻止当前窗口默认事件
+    e.preventDefault(e);
+    //把菜单模板添加到右键菜单
+    m.popup({window:remote.getCurrentWindow()});
+})
+
+
+//通过链接打开浏览器
+var { shell } = require('electron');
+
+var aHref = document.querySelector('#aHref');
+
+aHref.onclick = function(e){
+    e.preventDefault();
+    var href = this.getAttribute('href');
+    shell.openExternal(href);
+}
+
+
+//window.open打开子窗口
+var mybtn = document.querySelector('#mybtn');
+
+mybtn.onclick = function(e) {
+    window.open('./popup_page.html');
+}
+
+
+//父窗口接收信息
+window.addEventListener('message',(msg)=>{
+    let mytext = document.querySelector('#mytext');
+    mytext.innerHTML = JSON.stringify(msg.data);
+})
+
+
+//选择文件对话框
+const { dialog } = require('electron').remote;
+var openBtn = document.getElementById('openBtn');
+openBtn.onclick = function(){
+    dialog.showOpenDialog({
+        title:'请选择你喜欢的小姐姐照片',
+        defaultPath:'xiaojiejie.jpg',  //默认路径的设置
+        buttonLabel:'打开图片',  //自定义确定按钮
+        filters:[   //过滤器的使用
+            {name:'jpg',extensions:['jpg']}
+        ]
+    }).then(result=>{
+        let image = document.getElementById('images');
+        image.setAttribute("src",result.filePaths[0]);
+    })
+}
+
+
+//保存文件对话框
+const fs = require('fs');
+var saveBtn = document.getElementById('saveBtn');
+
+saveBtn.onclick = function(){
+    dialog.showSaveDialog({
+        title:'保存文件',
+    }).then(result=>{
+        console.log(result.filePath);
+        fs.writeFileSync(result.filePath,'技术胖一个前端爱好者');
+    }).catch(err=>{
+        console.log(err);
+    })
+}
