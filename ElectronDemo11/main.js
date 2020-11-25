9ef152ab8e62ae2e568d14efe3043377bd169539
tianshaojun <33224748+tianshaojun@users.noreply.github.com>
2020-11-25 13:37:29 +0800

    20201125

--- ElectronDemo11/main.js
+++ ElectronDemo11/main.js
@@ -8,26 +8,26 @@ var BrowserWindow = electron.BrowserWindow;
 
 var mainWindow = null;
 
-app.on('ready',()=>{
-    mainWindow = new BrowserWindow({width:800,height:600});
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({ width: 800, height: 600 });
 
-    globalShortcut.register('command+e',()=>{
+    globalShortcut.register('command+e', () => {
         mainWindow.loadURL('https://jspang.com');
     })
 
-    let isRegister = globalShortcut.isRegistered('command+e')?'Register Success':'Register fail';
+    let isRegister = globalShortcut.isRegistered('command+e') ? 'Register Success' : 'Register fail';
 
-    console.log('--------->'+isRegister);
+    console.log('--------->' + isRegister);
 
     mainWindow.loadFile('index.html');
 
     //监听关闭事件，把主窗口设置为null
-    mainWindow.on('closed',()=>{
+    mainWindow.on('closed', () => {
         mainWindow = null;
     })
 })
 
-app.on('will-quit',function(){
+app.on('will-quit', function () {
     //注销全局快捷键的监听
     globalShortcut.unregister('ctrl+e');
     globalShortcut.unregisterAll();
